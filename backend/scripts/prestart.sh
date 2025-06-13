8d849acabf1e771c49355ec140d1a9e04fb71783
Paweł Bielko <pavbialko@gmail.com>
2025-06-13 19:23:02 -0400

    Auto-recreate SQLite DB with 30 researchers on startup

--- backend/scripts/prestart.sh
+++ backend/scripts/prestart.sh
@@ -1,13 +1,10 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 set -e
 set -x
 
-# Let the DB start
-python app/backend_pre_start.py
+# Remove old SQLite database if it exists
+rm -f /app/app/database/scientists.db
 
-# Run migrations
-alembic upgrade head
-
-# Create initial data in DB
-python app/initial_data.py
+# Create and populate a new SQLite database
+python /app/scripts/fetch_and_create_scientists_db.py
